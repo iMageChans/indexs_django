959976130c23deb3809072a73e9277b132e68a4b
chans <imagechans@gmail.com>
2024-08-27 21:39:39 +0800

    update code

--- list/transfers/views.py
+++ list/transfers/views.py
@@ -80,35 +80,29 @@ class SwapTransferVolumeAPIView(APIView):
     def get(self, request, *args, **kwargs):
         now = timezone.now()
 
-        # 获取前一天和前两天的时间范围
         last_24_hours = now - timedelta(hours=24)
         last_48_hours = now - timedelta(hours=48)
 
-        # 获取前一天和前两天的数据
         transfers_last_24_hours = models.SwapTransfer.objects.filter(timestamp__gte=last_24_hours)
         transfers_last_48_to_24_hours = models.SwapTransfer.objects.filter(timestamp__gte=last_48_hours,
                                                                            timestamp__lt=last_24_hours)
 
-        # 计算前一天的交易数量和 USDT 总和
         transaction_count_last_24_hours = transfers_last_24_hours.count()
         usdt_sum_last_24_hours = sum(int(transfer.usdt) for transfer in transfers_last_24_hours if transfer.usdt)
 
-        # 计算前两天的交易数量和 USDT 总和
         transaction_count_last_48_to_24_hours = transfers_last_48_to_24_hours.count()
         usdt_sum_last_48_to_24_hours = sum(
             int(transfer.usdt) for transfer in transfers_last_48_to_24_hours if transfer.usdt)
 
-        # 计算交易数量的环比增长率
         if transaction_count_last_48_to_24_hours > 0:
             transaction_count_change_rate = ((transaction_count_last_24_hours - transaction_count_last_48_to_24_hours) / transaction_count_last_48_to_24_hours) * 100
         else:
-            transaction_count_change_rate = None  # 或者设置为 0, 表示无增长
+            transaction_count_change_rate = None
 
-        # 计算 USDT 总和的环比增长率
         if usdt_sum_last_48_to_24_hours > 0:
             usdt_sum_change_rate = ((usdt_sum_last_24_hours - usdt_sum_last_48_to_24_hours) / usdt_sum_last_48_to_24_hours) * 100
         else:
-            usdt_sum_change_rate = None  # 或者设置为 0, 表示无增长
+            usdt_sum_change_rate = None
 
         return Response({
             'transaction_count_last_24_hours': transaction_count_last_24_hours,
