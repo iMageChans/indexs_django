48f807cda6624f41f1ba147da24420bcaecb6a2b
chans <imagechans@gmail.com>
2024-08-27 21:01:26 +0800

    update code

--- list/transfers/urls.py
+++ list/transfers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from transfers.views import D9TransfersViewSet, USDTTransfersViewSet, SwapTransferVolumeAPIView
+from transfers.views import D9TransfersViewSet, USDTTransfersViewSet, SwapTransferVolumeAPIView, SwapTransferListViewSet
 
 
 urlpatterns = [
     path('transfers/d9/', D9TransfersViewSet.as_view(), name='transfers-d9'),
     path('transfers/usdt/', USDTTransfersViewSet.as_view(), name='transfers-usdt'),
+    path('swap/transfers/', SwapTransferListViewSet.as_view(), name='swap-transfer'),
     path('swap/transfers/day/', SwapTransferVolumeAPIView.as_view(), name='swap-transfer-24h-volume'),
 ]
